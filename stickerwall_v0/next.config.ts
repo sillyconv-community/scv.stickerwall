f856ecb33761853ad43b03099de3dd264d84587e
nicwong <wongchekhoi@hotmail.com>
2025-07-07 12:54:06 +0100

    update next.config.ts

--- stickerwall_v0/next.config.ts
+++ stickerwall_v0/next.config.ts
@@ -4,6 +4,9 @@ const isGithubPages = process.env.GITHUB_PAGES === "true";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: isGithubPages ? "/v0" : "",
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isGithubPages ? "/v0" : "",
+  }
   images: {
     unoptimized: true,
   },
