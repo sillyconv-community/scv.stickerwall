2d6f5dedd1838cca4343f6d7f0ba023c3194a5fa
nicwong <wongchekhoi@hotmail.com>
2025-07-07 11:48:15 +0100

    update next.config.ts

--- stickerwall_v0/next.config.ts
+++ stickerwall_v0/next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath isGithubPages ? '/repo-name': '',
+  basePath isGithubPages ? "/scv.stickerwall" : "v0",
   images: {
     unoptimized: true,
   },
